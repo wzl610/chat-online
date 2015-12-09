da2b708616b78d7584b6ffa59904f95cb7feee5d
wzl610 <237984496@qq.com>
2015-12-09 15:38:56 +0800

    添加点对点发送

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,7 @@
 var oChat = {
     socket : null,
     name : null,
+    toUser : null,
     logout:function(){
         $("#logout").on("click",function(){
             //登出操作
@@ -11,8 +12,17 @@ var oChat = {
         //信息发送
         $('.chat-button').on('click',function(){
             var msg = $('.chat-input').val();
-            oChat.socket.emit('msg',{'msg':msg,'name':oChat.name});
+            oChat.socket.emit('msg',{'msg':msg,'name':oChat.name,'to':oChat.toUser});
             $('.chat-input').val('');
+            var $msg = '<section class="mymsg"><div>'+msg+'</div><span>'+oChat.name+'</span></section>';
+            $('.chat').append($msg);
+            oChat.scrollToBottom();
+            toUser = null ; 
+        })
+    },
+    toSomeOne:function(){
+        $(".online-list").on('click','.online-link',function(){
+            oChat.toUser = $(this).text();
         })
     },
     scrollToBottom:function(){
@@ -21,17 +31,21 @@ var oChat = {
     //用户登入登出渲染操作
     updateMsg : function(type,obj){
         //type中0为退出,1为加入
-        $('.num').text(obj.onlineCount);
+        $('.num span').text(obj.onlineCount);
         var userList = '';
         for(var i in obj.onlineList){
-            userList += ','+obj.onlineList[i];
+            userList += '<li><a href="javascript:void(0);" class="online-link">'+obj.onlineList[i]+'</a></li>';
         }
-        userList = userList.slice(1);
-        $('.online-list').text(userList);
+        $('.online-list').empty().append(userList);
         var $msg = '<div class="msg-system">'+obj.user+(type==1?'加入':'退出')+'了聊天室</div>';
         $('.chat').append($msg);
         oChat.scrollToBottom();
     },
+    receiveMsg:function(obj){
+        var $msg = '<section class="othermsg"><span>'+obj.user+'</span><div>'+obj.msg+'</div></section>';
+        $('.chat').append($msg);
+        oChat.scrollToBottom();
+    },         
     init:function(){
         //初始化IO
         this.socket = io.connect('ws://localhost:3000');//如果用socket = io.connect('ws://localhost:3000')的话无效，创建的socket不是对象的socket变量
@@ -40,7 +54,7 @@ var oChat = {
         //发布信息告诉大家我登录了！
         this.socket.emit('login',{"name":this.name});
         //新用户登录消息通知
-        this.socket.on('login',function(obj){      
+        this.socket.on('login',function(obj){     
             oChat.updateMsg(1,obj);
         });
         //用户登出消息通知
@@ -48,27 +62,24 @@ var oChat = {
             oChat.updateMsg(0,obj);
         });
         //消息
-        this.socket.on('msg',function(obj){
-            var $msg = '';
-            if(oChat.name==obj.user){
-                //自己发的信息
-                $msg += '<section class="mymsg"><div>'+obj.msg+'</div><span>'+obj.user+'</span></section>';
-            }else{
-                //别人发的信息
-                $msg += '<section class="othermsg"><span>'+obj.user+'</span><div>'+obj.msg+'</div></section>';
-            }
-            $('.chat').append($msg);
-            oChat.scrollToBottom();
+         this.socket.on('msg',function(obj){
+            //别人群发信息
+            oChat.receiveMsg(obj);
         });
 
-        this.socket.on('message',function(obj){
+        this.socket.on("message",function(obj){
+            //别人单独对你发信息
+            oChat.receiveMsg(obj);
+        });
+
+        /*this.socket.on('message',function(obj){
             //当服务器使用socket.send时接收
             console.log(obj);
         });
 
         this.socket.on('other',function(obj){
             console.log('other person connect!');
-        })
+        })*/
     }/*,
     init : function(){
             var chat = io.connect('http://localhost:3000/chat'),
@@ -92,4 +103,5 @@ var oChat = {
 $(function(){
     oChat.init();
     oChat.sendMsg();
+    oChat.toSomeOne();
 })
